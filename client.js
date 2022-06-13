fc12a243722093f1c1ba068c2b065288ff15904e
pdev-gd <83932298+pdev-gd@users.noreply.github.com>
2022-06-13 16:12:23 +0900

    Create client.js

---
+++ client.js
@@ -0,0 +1,29 @@
+const { Client } = require('pg');
+
+const pgclient = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres'
+});
+
+pgclient.connect();
+
+const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
+const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
+const values = ['source', 'Kim', 20, 'Somewhere 211, Seoul, Republic of Korea', 'kimSource@github.com']
+
+pgclient.query(table, (err, res) => {
+    if (err) throw err
+});
+
+pgclient.query(text, values, (err, res) => {
+    if (err) throw err
+});
+
+pgclient.query('SELECT * FROM student', (err, res) => {
+    if (err) throw err
+    console.log(err, res.rows) 
+    pgclient.end()
+});
